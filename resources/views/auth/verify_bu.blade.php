46cbd9d64123dcb5e5b79a4d6c011b9cb020cb4b
Ali Seyfi <ali.seyfi68@gmail.com>
2019-10-26 23:08:03 +0330

    cleanups

--- resources/views/auth/verify_bu.blade.php
+++ resources/views/auth/verify_bu.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -22,3 +25,4 @@
         </div>
     </div>
 </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,5 @@
 
 Route::get('/', 'LandingController@index');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home');
