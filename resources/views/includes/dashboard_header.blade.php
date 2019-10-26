36ec82b0ef0c3efb792ef7c72f7bf1d6262dd384
Ali Seyfi <ali.seyfi68@gmail.com>
2019-10-27 01:44:47 +0330

    dashboard ready

--- resources/views/includes/dashboard_header.blade.php
+++ resources/views/includes/dashboard_header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
 	<div class="container-fluid">
 		<div class="navbar-wrapper">
-			<a class="navbar-brand" href="#pablo">داشبورد</a>
+			<a class="navbar-brand" href="{{url('/')}}"> <i class="material-icons">home</i> @lang('MAIN_PAGE') </a>
 		</div>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="sr-only">Toggle navigation</span>
@@ -12,7 +12,7 @@
 		<div class="collapse navbar-collapse justify-content-end">
 			<form class="navbar-form">
 				<div class="input-group no-border">
-					<input type="text" value="" class="form-control" placeholder="جستجو...">
+					<input type="text" value="" class="form-control" placeholder="@lang('SEARCH')...">
 					<button type="submit" class="btn btn-white btn-round btn-just-icon">
 						<i class="material-icons">search</i>
 						<div class="ripple-container"></div>
@@ -21,19 +21,19 @@
 			</form>
 			<ul class="navbar-nav">
 				<li class="nav-item">
-					<a class="nav-link" href="#pablo">
+					<a class="nav-link" href="{{url('home')}}">
 						<i class="material-icons">dashboard</i>
 						<p class="d-lg-none d-md-block">
-							آمارها
+							@lang('DASHBOARD')
 						</p>
 					</a>
 				</li>
 				<li class="nav-item dropdown">
-					<a class="nav-link" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						<i class="material-icons">notifications</i>
 						<span class="notification">۵</span>
 						<p class="d-lg-none d-md-block">
-							اعلان‌ها
+							@lang('NOTIFICATIONS')
 						</p>
 					</a>
 					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
@@ -44,13 +44,24 @@
 						<a class="dropdown-item" href="#">اعلان دیگر</a>
 					</div>
 				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#pablo">
+				<li class="nav-item dropdown">
+					<a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						<i class="material-icons">person</i>
 						<p class="d-lg-none d-md-block">
-							حساب کاربری
+							@lang('ACCOUNT')
 						</p>
 					</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+						<a class="dropdown-item" href="#">
+							<i class="material-icons mr-2">phonelink_lock</i> @lang('MANAGE_ACCOUNT')
+						</a>
+						<a class="dropdown-item" href="javascript:void" onclick="$('#logout-form').submit();">
+							<i class="material-icons mr-2">input</i> @lang('LOGOUT')
+						</a>
+					</div>
+					<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    					@csrf
+					</form>
 				</li>
 			</ul>
 		</div>
--- resources/views/includes/dashboard_sidebar.blade.php
+++ resources/views/includes/dashboard_sidebar.blade.php
@@ -1,13 +1,13 @@
-<div class="sidebar" data-color="purple" data-background-color="white" data-image="{{asset('dashboard/img/sidebar-1.jpg')}}">
-	<!--
+<div class="sidebar" data-color="purple" data-background-color="white" data-image="{{asset('dashboard/img/sidebar.jpg')}}">
+	{{-- <!--
 	Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
 
 	Tip 2: you can also add an image using data-image tag
--->
+--> --}}
 	<div class="logo">
-		<a href="http://www.creative-tim.com" class="simple-text logo-normal">
-		تیم خلاق
-	</a>
+		<a href="{{url('home')}}" class="simple-text logo-normal">
+			@lang('DASHBOARD')
+		</a>
 	</div>
 	<div class="sidebar-wrapper">
 		<ul class="nav">
