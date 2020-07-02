615007962307c99c84634098be7cb3c230d4b6a7
tihootech <ali.seyfi68@gmail.com>
2020-07-03 02:33:07 +0430

    bug fix

--- resources/views/landing/partials/donations.blade.php
+++ resources/views/landing/partials/donations.blade.php
@@ -1,16 +1,18 @@
-@foreach ($donations as $donation)
-    <div class="col-md-4">
-        <div class="card">
-            <div class="card-body">
-                <h4 class="text-primary"> {{$donation->name}} </h4>
-                <h5> <span class="calibri">{{number_format($donation->amount)}}</span> تومان </h5>
-                <hr>
-                <p class="text-rose"> {{$donation->info}} </p>
-                <p>
-                    <b class="text-primary"> پاسخ ادمین :  </b>
-                    <span class="text-muted"> {{$donation->reply}} </span>
-                </p>
+<div class="row">
+    @foreach ($donations as $donation)
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="text-primary"> {{$donation->name}} </h4>
+                    <h5> <span class="calibri">{{number_format($donation->amount)}}</span> تومان </h5>
+                    <hr>
+                    <p class="text-rose"> {{$donation->info}} </p>
+                    <p>
+                        <b class="text-primary"> پاسخ ادمین :  </b>
+                        <span class="text-muted"> {{$donation->reply}} </span>
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-@endforeach
+    @endforeach
+</div>
